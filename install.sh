b28eab754ab6a4c58dba03bffcffca78e3f02fc7
ks-chuya <u461951j@ecs.osaka-u.ac.jp>
2024-04-04 16:07:49 +0900

    Add OS check

--- install.sh
+++ install.sh
@@ -1,9 +1,20 @@
 #!/bin/bash -v
-if   [ -e /c/Users/$(whoami)/AppData/Local/Programs/arduino-ide]; then
-    curl 
-elif [ -e /c/Users/$(whoami)/AppData/Local/Programs/Arduino\ IDE]; then
-    curl 
-else
-    echo TAを呼んでください．
+OS="$(uname)"
+if [ "${OS}" == "Linux"]
+    # linux
+elif ["${OS}" == "Darwin"]
+    # macOS
+else 
+    # plugins folderにいく
+    if   [ -e /c/Users/$(whoami)/AppData/Local/Programs/arduino-ide]; then
+        cd /c/Uesrs/$(whoami)/AppData/Local/Programs/arduino-ide/resources/app/plugins | echo TAを読んでください
+    elif [ -e /c/Users/$(whoami)/AppData/Local/Programs/Arduino\ IDE]; then
+        cd /c/Uesrs/$(whoami)/AppData/Local/Programs/Arduino\ IDE/resources/app/plugins | echo TAを読んでください
+    else
+        echo TAを呼んでください．
+    fi
+
+    curl # release から ひっぱってくる
+    tar -xaf file.tar.gz && rm file.tar.gz
 fi
 
