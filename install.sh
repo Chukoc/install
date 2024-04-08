160339424bae5de89703a2c8521abe9b4699b0ea
Chukoc <u461951j@ecs.osaka-u.ac.jp>
2024-04-08 19:43:26 +0900

    fix

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -v
 OS="$(uname)"
-if ["${OS}" == "Darwin"]
+if [ "${OS}" == "Darwin" ]; then
     # macOS
     cd /Application/Arduino\ IDE.app/Contents/Resources/app/plugins
     if [ $? -ne 0 ]; then
@@ -11,14 +11,15 @@ if ["${OS}" == "Darwin"]
     tar -xzf file.tar.gz && rm file.tar.gz
 else 
     # plugins folderにいく
-    if   [ -e /c/Users/$(whoami)/AppData/Local/Programs/arduino-ide]; then
+    if   [ -e /c/Users/$(whoami)/AppData/Local/Programs/arduino-ide ]; then
         cd /c/Uesrs/$(whoami)/AppData/Local/Programs/arduino-ide/resources/app/plugins 
         if [ $? -ne 0 ]; then
             echo TAを読んでください
             exit 1
         fi
-    elif [ -e /c/Users/$(whoami)/AppData/Local/Programs/Arduino\ IDE]; then
-        cd /c/Uesrs/$(whoami)/AppData/Local/Programs/Arduino\ IDE/resources/app/plugins 
+    elif [ -e /c/Users/$(whoami)/AppData/Local/Programs/Arduino\ IDE ]; then
+        # cd C:/Uesrs/$(whoami)/AppData/Local/Programs/Arduino\ IDE/resources/app/plugins 
+        cd /c/Users/$(whoami)/AppData/Local/Programs/Arduino\ IDE/resources/app/plugins
         if [ $? -ne 0 ]; then
             echo TAを読んでください 
             exit 1
@@ -28,7 +29,7 @@ else
         exit 1
     fi
 
-    curl # release から ひっぱってくる
-    tar -xaf file.tar.gz && rm file.tar.gz
+    curl -sLJO https://github.com/Chukoc/install/releases/download/Test/ino-smell-detector-win.tar.gz
+    tar -xaf ino-smell-detector-win.tar.gz && rm ino-smell-detector-win.tar.gz
 fi
 
