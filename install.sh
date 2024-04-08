6f6ff0599ee8b233008c223898d10569c8d75433
Chukoc <u461951j@ecs.osaka-u.ac.jp>
2024-04-08 16:16:08 +0900

    Add err

--- install.sh
+++ install.sh
@@ -4,14 +4,30 @@ if [ "${OS}" == "Linux"]
     # linux
 elif ["${OS}" == "Darwin"]
     # macOS
+    cd /Application/Arduino\ IDE.app/Contents/Resources/app/plugins
+    if [ $? -ne 0 ]; then
+        echo TAを読んでください
+        exit 1
+    fi
+    curl
+    tar -xzf file.tar.gz && rm file.tar.gz
 else 
     # plugins folderにいく
     if   [ -e /c/Users/$(whoami)/AppData/Local/Programs/arduino-ide]; then
-        cd /c/Uesrs/$(whoami)/AppData/Local/Programs/arduino-ide/resources/app/plugins | echo TAを読んでください
+        cd /c/Uesrs/$(whoami)/AppData/Local/Programs/arduino-ide/resources/app/plugins 
+        if [ $? -ne 0 ]; then
+            echo TAを読んでください
+            exit 1
+        fi
     elif [ -e /c/Users/$(whoami)/AppData/Local/Programs/Arduino\ IDE]; then
-        cd /c/Uesrs/$(whoami)/AppData/Local/Programs/Arduino\ IDE/resources/app/plugins | echo TAを読んでください
+        cd /c/Uesrs/$(whoami)/AppData/Local/Programs/Arduino\ IDE/resources/app/plugins 
+        if [ $? -ne 0 ]; then
+            echo TAを読んでください 
+            exit 1
+        fi
     else
-        echo TAを呼んでください．
+        echo TAを呼んでください． 
+        exit 1
     fi
 
     curl # release から ひっぱってくる
