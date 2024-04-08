06f425eec2d65f78880fa3fd93b155d48e5a325d
ks-chuya <u461951j@ecs.osaka-u.ac.jp>
2024-04-08 11:00:47 +0900

    Add mac

--- install.sh
+++ install.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -v
 OS="$(uname)"
-if [ "${OS}" == "Linux"]
-    # linux
-elif ["${OS}" == "Darwin"]
+if ["${OS}" == "Darwin"]
     # macOS
+    cd /Applications/Arduino\ IDE.app/Contents/Resources/app/plugins | echo TAを呼んでください
+    curl # release から ひっぱってくる
+    tar -xzf file.tar.gz && rm file.tar.gz
 else 
     # plugins folderにいく
     if   [ -e /c/Users/$(whoami)/AppData/Local/Programs/arduino-ide]; then
-        cd /c/Uesrs/$(whoami)/AppData/Local/Programs/arduino-ide/resources/app/plugins | echo TAを読んでください
+        cd /c/Uesrs/$(whoami)/AppData/Local/Programs/arduino-ide/resources/app/plugins | echo TAを呼んでください
     elif [ -e /c/Users/$(whoami)/AppData/Local/Programs/Arduino\ IDE]; then
-        cd /c/Uesrs/$(whoami)/AppData/Local/Programs/Arduino\ IDE/resources/app/plugins | echo TAを読んでください
+        cd /c/Uesrs/$(whoami)/AppData/Local/Programs/Arduino\ IDE/resources/app/plugins | echo TAを呼んでください
     else
         echo TAを呼んでください．
     fi
